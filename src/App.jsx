fd7f3e0ca9f70fbf8ccccb94469e48ddca291fed
Billy <ismail.bilkisu@gmail.com>
2023-10-03 14:52:24 +0100

    header created

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import SignOut from './pages/SignOut'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-out' element={<SignOut />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
